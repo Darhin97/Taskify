899f35345931168d563c1c3fddd4f795386fe350
Derrick Ankrah <darhin97@gmail.com>
2024-02-04 00:08:58 +0000

    added copy list, delete list

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -7,6 +7,7 @@ import { FormInput } from "@/components/form/form-input";
 import { useAction } from "@/hooks/use-action";
 import { updateList } from "@/actions/update-list";
 import { toast } from "sonner";
+import { ListOptions } from "@/app/(platform)/(dashboard)/board/[boardId]/_components/list-options";
 
 interface ListHeaderProps {
   data: List;
@@ -100,6 +101,8 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
           {data.title}
         </div>
       )}
+
+      <ListOptions data={data} onAddCard={() => {}} />
     </div>
   );
 };
